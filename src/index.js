c986505dd6eadc970f82e93149b7236622b84490
imvetri <somethingelse@web.com>
2018-12-23 19:31:21 +0530

    Imports codemirror as a react component, Adds Components,Elements.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import style from "./hello.css";
+// Libraries.
 
-const Index = () => {
-    return <div className={style.showBackground}>Hello Word</div>
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+// Dependencies.
+
+import style from "./style.css";
+
+// Components.
+
+import Components from "./components/Components";
+import Elements from "./elements/Elements";
+import CodeMirror from "react-codemirror";
+
+class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { code: "// Code" };
+    }
+    updateCode (newCode) {
+		this.setState({
+			code: newCode,
+		});
+    }
+    render(){
+        const options = {
+			lineNumbers: true,
+		};
+        return (
+            <div className={style.showBackground}>
+                <Elements/>
+                <Components/>
+                <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<Index/>, document.getElementById("index"));
