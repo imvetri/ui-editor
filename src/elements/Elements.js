54882a4e2c596cc84b8cc6b0b5190470f8cb0e1a
imvetri <somethingelse@web.com>
2018-12-30 17:15:09 +0530

    Moves codemirror into elements. Work only on elements tab for now

--- src/elements/Elements.js
+++ src/elements/Elements.js
@@ -1,34 +1,67 @@
 
+// Libraries.
+
 import React, { Component } from 'react';
 
+// Styles.
+
+import style from "./element.css";
+
+// Dependencies.
+
+import CodeMirror from "react-codemirror";
+
 class Elements extends Component {
-  render() {
-    return (
-      <li className="elements">
-          <header>Elements</header>
-          <section className="element-list">
-              <ul>
-                  <li>dummy element</li>
-              </ul>
-              <button id="addElement">Add</button>
-          </section>
-          <section className="events-tab">
-              <header>Events</header>
-              <ul>
-                  <li>dummy event</li>
-              </ul>
-              <button id="addEvent">Add</button>
-          </section>
-          <section className="states-tab">
-              <header>States</header>
-              <ul>
-                  <li>dummy state</li>
-              </ul>
-              <button id="addElementState">Add</button>
-          </section>
-      </li>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { code: "// Code", hideCodeEditor: true };
+    }
+
+    updateCode (newCode) {
+		this.setState({
+			code: newCode,
+        });
+    }
+
+    toggleEditor () {
+        this.setState({
+            hideCodeEditor: false
+        });
+    }
+
+    render() {
+        
+        const options = {
+			lineNumbers: true,
+        };
+        
+        return (
+            <li className="elements">
+                <header>Elements</header>
+                <section className="element-list">
+                    <ul>
+                        <li>dummy element</li>
+                    </ul>
+                    <button id="addElement" onClick={this.toggleEditor.bind(this)}>Add</button>
+                    <CodeMirror className={this.state.hideCodeEditor ? style.hidden : ''} value={this.state.code} onChange={this.updateCode.bind(this)} options={options} />
+                </section>
+                <section className="events-tab">
+                    <header>Events</header>
+                    <ul>
+                        <li>dummy event</li>
+                    </ul>
+                    <button id="addEvent">Add</button>
+                </section>
+                <section className="states-tab">
+                    <header>States</header>
+                    <ul>
+                        <li>dummy state</li>
+                    </ul>
+                    <button id="addElementState">Add</button>
+                </section>
+            </li>
+        );
+    }
 }
 
 export default Elements;
