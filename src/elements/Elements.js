8cc335fce6144472f804af478f504327ff746264
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-01-10 17:17:44 +0530

    Adds documentation to element.js and popup-editor. Next add react-json-view for states

--- src/elements/Elements.js
+++ src/elements/Elements.js
@@ -26,80 +26,134 @@ class Elements extends Component {
         };
     }
 
+    /**
+     * Shows / hides the pop up editor.
+     */
     toggleEditor () {
         this.setState({
             createMode: !this.state.createMode
         });
     }
 
+    /**
+     * Updates state with a new element / edited element.
+     * @param {Object} newElement 
+     * @property {String} name - The name of the element.
+     * @property {String} markup - The markup of the element.
+     * @property {Array} events - The events of the element. Defaults to [].
+     * @property {Array} states - The states of the element. Defaults to [].
+     */
     updateCode (newElement) {
         // Mutate the original array. Science fiction. hide mutation behind.
         let newList = Array.from(this.state.list);
         
         if(this.state.editMode){
             // Find the element.
-            // Update the element with new markup.
             const elementUnderEdit = newList.find(item=> item.name === newElement.name || item.markup === newElement.markup);
+
+            // Update the element with new markup.
             elementUnderEdit.markup = newElement.markup;
 
-            let newState = convertToJson(extractJsxAttributes(newElement.markup));
-            elementUnderEdit.states.push( newState );
+            // Get default state by parsing the markup.
+            let defaultState = convertToJson(extractJsxAttributes(newElement.markup));
+
+            // Add default state to element's states.
+            elementUnderEdit.states.push( defaultState );
+
+            // Update element name.
             elementUnderEdit.name = newElement.name;
         }
         else {
             newList.push(newElement);
         }
 
-        // Update the state with new values
+        // Update the state with new values.
         this.setState({
             list: newList
-        })
+        });
+
+        // hide the editor.
         this.toggleEditor();
     }
 
+    /**
+     * Stores the new event name created for the currently selected element.
+     * @param {Object} e - Event carrying infor about the eventName input value.
+     */
     updateEventName (e) {
         this.setState({
             eventName: e.target.value
         })
     }
 
+    /**
+     * Saves the event name which is created for the currently selected element.
+     */
     addEvent () {
-        // this seems wrong.
+        // Get the selected element.
         let selectedElement = this.state.selectedElement;
 
+        // Create new state.
         let newState = Object.assign({}, this.state);
+
+        // Find the element to be updated from the new state.
         let elementToBeUpdated = newState.list.find(element=>element.name === selectedElement.name);
 
+        // Update the event name to the selectedElement.
         selectedElement.events.push(this.state.eventName);
+
+        // Set state to the new state.
         this.setState(newState);
     }
 
+    /**
+     * Stores the new state name for the currently selected element.
+     * @param {Object} e - Event carrying info about the state input value.
+     */
     updateStateName (e) {
         this.setState({
             stateName: e.target.value
         })
     }
 
+    /**
+     * Saves the stateName to the currently selected element.
+     */
     addState () {
-        // this seems wrong.
+        // Get the selected element.
         let selectedElement = this.state.selectedElement;
 
+        // Create new state.
         let newState = Object.assign({}, this.state);
+
+        // Find the element to be updaated from the new state.
         let elementToBeUpdated = newState.list.find(element=>element.name === selectedElement.name);
 
+        // Update the state name to list of existing states.
         selectedElement.states.push(this.state.stateName);
+        
+        // Set state to new state.
         this.setState(newState);
     }
 
+    /**
+     * Stores the current element that is selected.
+     * @param {Object} e - Event carrying info about the currently selected element.
+     */
     updateSelectedElement (e) {
-
+        // Find the element from state that matches the currently selected element.
         let selectedElement = this.state.list.find(element=>element.name === e.target.innerText)
+
+        // Update the state with selectedElement.
         this.setState({
             selectedElement
         })
 
     }
 
+    /**
+     * Shows pop up editor.
+     */
     editElementMarkup () {
         this.setState({
             editMode: true
@@ -129,8 +183,6 @@ class Elements extends Component {
         const stateList = this.state.selectedElement && this.state.selectedElement.states.map((state, index)=>
             <li key={index}>{JSON.stringify(state)}</li>
         );
-
-        console.log(stateList);
 
         const editMarkup = this.state.selectedElement ?  <button onClick={this.editElementMarkup.bind(this)} >Edit</button> : "";
 
