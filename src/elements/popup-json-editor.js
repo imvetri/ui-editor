e158c2eb66469425da7a783f8269519296826f75
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-01-11 13:26:41 +0530

    Shows/hides json editor. Next show states in tree view in the list, when clicked open the json popup eidtor

--- src/elements/popup-json-editor.js
+++ src/elements/popup-json-editor.js
@@ -2,6 +2,7 @@
 // Libraries.
 
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 // Dependencies.
 
@@ -10,17 +11,18 @@ import CodeMirror from "react-codemirror";
 
 // Styles.
 import elementStyle from "./element.css";
-
+import cssUtil from "../css-utils/style.css";
 
 class PopupJsonEditor extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            json: this.props.json || {}
+        this.state = {
+            json: this.props.json || {},
+            show: this.props.show
         }
     }
 
-    updateJson (json) {
+    updateJson(json) {
         this.setState({
             json: JSON.parse(json)
         })
@@ -34,9 +36,11 @@ class PopupJsonEditor extends Component {
         };
 
         return (
-            <div className={elementStyle.override}>
-                <CodeMirror value={JSON.stringify(this.state.json)} onChange={this.updateJson.bind(this)} options={options} />
-                <ObjectInspector data={this.state.json} />
+            <div className={this.props.show || this.state.show ?  '' : cssUtil.hidden}>
+                <div className={elementStyle.override}>
+                    <CodeMirror value={JSON.stringify(this.state.json)} onChange={this.updateJson.bind(this)} options={options} />
+                    <ObjectInspector data={this.state.json} />
+                </div>
             </div>
         );
     }
