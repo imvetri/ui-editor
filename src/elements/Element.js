80e5882fdb04ae04fa16401f61ed1a163bcbcdce
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-04-01 00:44:05 +0530

    Refactored elements and element

---
+++ src/elements/Element.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+// Styles.
+
+import style from "./Style.css";
+
+// Dependencies.
+
+import {updateselectedIndex} from "./Reducer";
+
+class Element extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    selectionChanged(e) {
+        this.props.onSelectionChange(e);
+    }
+
+    previewElement(e) {
+        this.props.onPreview(e)
+    }
+
+    render() {
+
+        return (
+            <li 
+                className = {this.state.selectedIndex === this.props.index ? style.selected : ""}
+                onClick = {this.selectionChanged.bind(this)}>
+                {this.props.element.name}
+                <button onClick={this.previewElement.bind(this)}>Preview</button>
+            </li>
+        );
+    }
+}
+
+export default Element;
