08694e4f6931571fc62e16a3faf0ecd92047b3b0
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-04-19 17:48:06 +0530

    Introduce code mock structure for exported components. Next manually create a component.js file using the element data and check whether it can be imported into other components and works as expexted

---
+++ src/mock/ExportedComponent/Component.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+// Styles.
+
+import style from "./Style.css";
+
+class Element extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+
+    /*
+    Event callbacks here.
+    */
+
+    render() {
+
+        return (
+
+            // Markup here.
+            // For all the events, 
+                // Find the tag name. - Tag 
+                // Add string Event name = {this.event.reducer.bind(this)}
+
+        );
+    }
+}
+
+export default Element;
