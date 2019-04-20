81300315b8d57dc2b2116e377ee78a826180115c
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-04-20 09:39:30 +0530

    Console.logs the element as a react component. Next add styles and test a component style changing for an event. Later, download style file

---
+++ src/common/js/convert-to-react-component.js
@@ -0,0 +1,76 @@
+// Elements to  react component.
+const convertToReactcomponent = (element)=>{
+
+    let getComponentEventedMarkup = (markup, events)=>{
+        events.forEach(event=>{
+            let id = `id="ID${event.id.split("ID")[1]}"`;
+            markup = markup.replace(id, `${id} ${event.name}={this.${event.id+event.name}.bind(this)}`);
+        });
+    
+        return markup.split("{state.").join("{this.state.")
+    
+        return markup;
+    }
+    
+    let getComponentReducers = (events) => {
+        return events.map(event=>{
+            let functionName = event.id+event.name;
+            let functionDef = event.reducer;
+            return `
+    ${functionName} () {
+    ${functionDef}
+    }`
+        }).join("\n")
+    }
+    
+    
+    // element = {
+    //   name: "Nested Component",
+    //   markup: "<div id=\"ID1\">\n<span id=\"ID2\">{state.name}</span>\n<button id=\"ID3\">{state.text}</button>\n</div>",
+    //   events: [
+    //     {
+    //       name: "onClick",
+    //       reducer: "this.setState({\n\"name\":\"lala\"\n})",
+    //       id: "spanID2"
+    //     },
+    //     {
+    //       name: "onClick",
+    //       reducer: "this.setState({\n\"text\":\"VEL\"\n})",
+    //       id: "buttonID3"
+    //     }
+    //   ],
+    //   state: "{\n\"name\":\"vetri\",\n\"text\":\"vel\"\n}"
+    // }
+    
+    
+    let componentEventedMarkup = getComponentEventedMarkup(element.markup, element.events)
+    let componentReducers = getComponentReducers(element.events)
+    let componentName = element.name.split(" ").join("")
+    let componentState = element.state
+    let ReactComponent = 
+    `
+    import React, { Component } from 'react';
+    class ${componentName} extends Component {
+    
+        constructor(props) {
+            super(props);
+            this.state = ${componentState};
+        }
+    
+        ${componentReducers}
+    
+        render() {
+    
+            return (${componentEventedMarkup})
+        }
+    }
+    
+    export default ${componentName};
+    
+    `
+    return ReactComponent;
+}
+
+module.exports = {
+    convertToReactcomponent
+}
