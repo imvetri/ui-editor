415a29c3e490fe4f430d4de45108229729ddf6d8
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-04-22 15:47:13 +0530

    style changes - small font-sizes

--- src/Nodes/Nodes.js
+++ src/Nodes/Nodes.js
@@ -13,25 +13,29 @@ class Nodes extends Component {
             var children = node.props.children.map(child=><Nodes node={child} onSelectedElementChanged={this.props.onSelectedElementChanged}/>);
             return (
                 <ul>
-                    <input 
-                        type="radio" 
-                        name="selectedElement" 
-                        onChange={this.props.onSelectedElementChanged} 
-                        value={node.type+ node.props.id}/>
-                    {node.type + node.props.id}
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="selectedElement" 
+                            onChange={this.props.onSelectedElementChanged} 
+                            value={node.type+ node.props.id}/>
+                        {node.type + node.props.id}
+                    </label>
                     {children}
                 </ul>
             );
         }
         return (
             <ul>
-                <input 
-                    type="radio" 
-                    name="selectedElement" 
-                    value={node.type+ node.props.id}
-                    onChange={this.props.onSelectedElementChanged} 
-                    />
-                {node.type + node.props.id}
+                <label>
+                    <input 
+                        type="radio" 
+                        name="selectedElement" 
+                        value={node.type+ node.props.id}
+                        onChange={this.props.onSelectedElementChanged} 
+                        />
+                    {node.type + node.props.id}
+                </label>
             </ul>
         );
     }
