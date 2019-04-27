991bb98fec48f4b8d4a5685a5f5f4807fb027554
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-04-27 19:46:34 +0530

    Remvoes unwated css compilation

--- src/common/js/jsxTranspiler/index.js
+++ src/common/js/jsxTranspiler/index.js
@@ -1,11 +1,8 @@
-import {createStylesheet} from "../create-stylesheet";
 import {compileJSX} from "../compile-jsx";
 
 // IMPORTANT - Do not rename style,state,events. 
 
 export function transpileJSX(jsx, style, state, events) {
-
-    createStylesheet(style);
 
     return compileJSX(jsx, style, state, events);
 }
