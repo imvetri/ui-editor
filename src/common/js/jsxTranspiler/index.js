b1386a45fce57197ca5c62f2f5194d28b56e4e18
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-04-27 19:53:22 +0530

    REmoves jsxTranspiler

--- src/common/js/jsxTranspiler/index.js
+++
@@ -1,8 +0,0 @@
-import {compileJSX} from "../compile-jsx";
-
-// IMPORTANT - Do not rename style,state,events. 
-
-export function transpileJSX(jsx, style, state, events) {
-
-    return compileJSX(jsx, style, state, events);
-}
