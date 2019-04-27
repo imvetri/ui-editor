f5d6e9596588fdd7726753fa56ad59ac751f3595
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-04-27 19:40:16 +0530

    Refactor - jsx compiler

---
+++ src/jsxTranspiler/store-events-global/index.js
@@ -0,0 +1,22 @@
+
+
+// IMPORTANT - Do not rename style,state,events. 
+
+export function storeEventsGlobal(events) {
+
+    // Automatically create ID creation rather than making user to include it.
+    events.forEach(event => {
+        // Fetch the id from event
+        let id = "ID"+event.id.split("ID")[1]
+
+        // Store the event.
+        /**
+         *  Example
+         * window.event123 = {
+         *      onClick: event.reducer
+         * }
+         * */
+        window["event"+id] = window["event"+id] || {};
+        window["event"+id][event.name] = event.reducer;
+    });
+}
