290a2349f6116f3bf9444d4aa9188a7b94c277a0
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-05-02 12:49:49 +0530

    Adds message component for preview info

--- src/Event/Index.js
+++ src/Event/Index.js
@@ -41,6 +41,10 @@ class Event extends Component {
             let messages = [{
                 text:"INFO: Select any tag to bind events from the Events tab",
                 type:"info"
+            },
+            {
+                text:"INFO: Click 'Preview' to see the component rendered on right most pane(Preview pane)",
+                type:"info"
             }]
             return (
                 <div>
