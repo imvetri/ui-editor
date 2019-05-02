9767220ccc951b13495ad756ff42972a578d2481
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-05-02 23:50:42 +0530

    Handle error in node creation when there are only one tag, events now throw error if jsx transpilation does not occur as exepected

--- src/Nodes/Index.js
+++ src/Nodes/Index.js
@@ -9,8 +9,8 @@ class Nodes extends Component {
     }
     render() {
 
-        // Elements needs to be transpiled properly. JSX did not render properly?
         var node = this.props.node;
+        // Check if it contains children.
         if(node.props && Array.isArray(node.props.children)){
             var children = node.props.children.map((child,index)=><Nodes key={index} node={child} onSelectedElementChanged={this.props.onSelectedElementChanged}/>);
             return (
@@ -24,6 +24,23 @@ class Nodes extends Component {
                         {node.type + node.props.id}
                     </label>
                     {children}
+                </ul>
+            );
+        }
+        // if node contains only one children, jsx get transpiled to object rather than array.
+        else if(typeof node.props.children === "object"){
+            let child = node.props.children;
+            return (
+                <ul>
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="selectedElement" 
+                            onChange={this.props.onSelectedElementChanged} 
+                            value={node.type+ node.props.id}/>
+                        {node.type + node.props.id}
+                    </label>
+                    <Nodes key={index} node={child} onSelectedElementChanged={this.props.onSelectedElementChanged}/>
                 </ul>
             );
         }
