6e7273a31c45bd0571971d3735383bbc9c381fed
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-05-21 20:43:08 +0530

    elements tab style changes

--- src/Element/Index.js
+++ src/Element/Index.js
@@ -25,19 +25,21 @@ class Element extends Component {
     render() {
         // Remove this.props.index, instead use this element instance index. Removes duplicate code
         return (
-            <li 
-                className = {this.props.selectedIndex === this.props.index ? style.selected : ""}
-                onClick = {this.selectionChanged.bind(this)}
-                index = {this.props.index}>
-                {this.props.element.name}
-                <button 
-                    index = {this.props.index} 
-                    onClick={this.previewElement.bind(this)} className="preview">Preview</button>
-                <button 
-                    index = {this.props.index}
-                    onClick={this.props.onExport}>Export</button>
-                <button disabled={this.props.selectedIndex === this.props.index ? "" : "disabled"}>{this.props.selectedIndex === this.props.index ? "Active" : "Inactive"}</button>
-            </li>
+            <div className={style.background}>
+                <li 
+                    className = {this.props.selectedIndex === this.props.index ? style.selected : ""}
+                    onClick = {this.selectionChanged.bind(this)}
+                    index = {this.props.index}>
+                    {this.props.element.name}
+                    <button 
+                        index = {this.props.index} 
+                        onClick={this.previewElement.bind(this)} className="preview">Preview</button>
+                    <button 
+                        index = {this.props.index}
+                        onClick={this.props.onExport}>Export</button>
+                    <button disabled={this.props.selectedIndex === this.props.index ? "" : "disabled"}>{this.props.selectedIndex === this.props.index ? "Active" : "Inactive"}</button>
+                </li>
+            </ div>
         );
     }
 }
