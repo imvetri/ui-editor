f4c6026807a2f58f34fa7ef167c012c9fb57d551
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-05-22 12:02:22 +0530

    Rename selected tag chagned event

--- src/Events/Index.js
+++ src/Events/Index.js
@@ -64,7 +64,7 @@ class Events extends Component {
         this.props.onEventsUpdate(element.events);
     }
 
-    selectedElementChanged(e){
+    selectedTagChanged(e){
         this.setState({
             selectedElement: e.currentTarget.value
         })
@@ -102,7 +102,7 @@ class Events extends Component {
                 <div className={style.events}>
                 <h4>Events, Actions, Reducers</h4>
                 <p>Select a tag below to show/add the events.</p>
-                <Nodes node={nodeTree.result} onSelectedElementChanged={this.selectedElementChanged.bind(this)}/>
+                <Nodes node={nodeTree.result} onSelectedTagChanged={this.selectedTagChanged.bind(this)}/>
                 {getHelp()}
             </div>
             )
@@ -111,7 +111,7 @@ class Events extends Component {
             <div className={style.events}>
                 <h4>Events, Actions, Reducers</h4>
                 <p>Select a tag below to show/add the events.</p>
-                <Nodes node={nodeTree.result} onSelectedElementChanged={this.selectedElementChanged.bind(this)}/>
+                <Nodes node={nodeTree.result} onSelectedTagChanged={this.selectedTagChanged.bind(this)}/>
                 {getHelp()}
                 <div className={style.existingEvents}>
                     <h5>Existing Events</h5>
