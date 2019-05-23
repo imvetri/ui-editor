d2031012ba7d724073b0468e6528015ba0e7a057
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-05-23 13:54:35 +0530

    Cleanup, jsxTranspiler, move availNesteccomponent outside

--- src/utilities/jsxTranspiler/index.js
+++ src/utilities/jsxTranspiler/index.js
@@ -3,40 +3,6 @@ import {storeEventsGlobal } from "./store-events-global";
 import {createStylesheet} from "./create-stylesheet";
 import {getNodeTree} from "../get-node-tree";
 
-import DynamicComponent from "../../DynamicComponent";
-
-function checkNestedComponents(components, markup) {
-    return components.filter(component=> markup.includes(component.name)).length >0;
-}
-
-function saveToWindow( component ) {
-    let randomKey = component.id*(~~(Math.random()*10));
-    window[component.name] = <DynamicComponent key={randomKey} component={component}/>
-    
-    // un comment next line, events will show nested component's events
-    // window[component.name] = transpileJSX(component.markup, component.style, component.state, component.events).result;
-
-}
-
-function saveComponentsToWindow( nestedComponents){
-    // Transpile them and make them global.
-    nestedComponents.forEach(saveToWindow);
-}
-
-export function availNestedComponent ( element ) {
-
-    // Should be able to detect nested component.
-
-    let components= JSON.parse(localStorage.getItem("ui-editor"));
-
-    if(checkNestedComponents(components, element.markup)){
-        // find all the nested components from the markup.
-        let nestedComponents = components.filter(component=> element.markup.includes(component.name));
-        
-        saveComponentsToWindow(nestedComponents);
-    }
-
-}
 // IMPORTANT - Do not rename style,state,events. 
 
 export function transpileJSX(markup, style, state, events) {
