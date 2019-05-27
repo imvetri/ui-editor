0eedb576d1c910c52168c69313ca8f03d93dd50d
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-05-27 17:45:10 +0530

    Commit - working nested components but code cleanup made it horrible

--- src/utilities/codeGenerator/prepareElement.js
+++ src/utilities/codeGenerator/prepareElement.js
@@ -1,11 +1,11 @@
 import {prepareMarkup} from "./prepareMarkup";
 
-export function prepareElement(element) {
+export function prepareElement(element, name) {
     let state = {};
 
 
     // touch this code to add markup, events, state.
-    element.markup = prepareMarkup(element);
+    element.markup = prepareMarkup(element, name);
 
     let defaults = {
         state: state,
