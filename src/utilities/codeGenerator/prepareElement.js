04b49f8dd90e6c2c42e03d284e008ceb5df98c0d
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-05-28 12:57:21 +0530

    Prepare markup to be cleaned up

--- src/utilities/codeGenerator/prepareElement.js
+++ src/utilities/codeGenerator/prepareElement.js
@@ -1,14 +1,7 @@
-import {prepareMarkup} from "./prepareMarkup";
-
-export function prepareElement(element, name) {
-    let state = {};
-
-
-    // touch this code to add markup, events, state.
-    element.markup = prepareMarkup(element, name);
+export function prepareElement(element) {
 
     let defaults = {
-        state: state,
+        state: {},
         style: {}
     };
     return Object.assign(defaults, element);
