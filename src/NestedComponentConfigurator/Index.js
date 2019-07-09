3fff62a532440ba029d0e7887e165765dcd308d9
Shanmugam, Vetrivel <svetrivel.91@gmail.com>
2019-07-09 12:19:08 +0530

    Adds show,hide condition for a nested component. Next show the nestedcomponent configurator only when a child component is selected

--- src/NestedComponentConfigurator/Index.js
+++ src/NestedComponentConfigurator/Index.js
@@ -11,12 +11,32 @@ class NestedComponentConfigurator extends Component {
         this.state = {}
     }
 
+    updateShowCondition () {
+
+    }
+
+    updateHideCondition () {
+
+    }
+
     render() {
 
-        debugger;
+        let components= JSON.parse(localStorage.getItem("ui-editor"));
+
         return (
             <div className={style.event}>
                 {this.props.childComponents.map(component=><input type="text" value={component}/>)}
+                <label>
+                Show.
+                <input type="text" onChange={this.updateShowCondition.bind(this)} value={this.state.showCondition} placeholder="Enter show condition name" title="Ex: state.title==='hey'; expression should eval to boolean"/>
+                </label>
+                <label>
+                Hide.
+                <input type="text" onChange={this.updateHideCondition.bind(this)} value={this.state.hideCondition} placeholder="EnExisting Eventster hide condition name" title="Ex: state.title==='hello'; expression should eval to boolean"/>
+                </label>
+                <label>
+                    List of publishable events of the component.
+                </label>
             </div>
         );
     }
