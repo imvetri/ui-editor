00642ad424f8203dd4b5bc62bf2d3ee1b1058bca
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-08-23 10:59:38 +0530

    Cleanup dynamic component

--- src/DynamicComponent/Index.js
+++ src/DynamicComponent/Index.js
@@ -1,15 +1,11 @@
 // Libraries.
 
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 
-import { codeModifier } from "../utilities/codeModifier";
 import {createStylesheet} from "../utilities/jsxTranspiler/create-stylesheet";
 import {createComponent} from "../utilities/convert-to-react-component";
-import { saveComponentsToWindow, getNestedComponents } from "../utilities/nestedComponentSetup";
 
 import style from "./style.css";
-import getMessages from "./Messages";
 
 class DynamicComponent extends Component {
     constructor(props) {
@@ -24,19 +20,11 @@ class DynamicComponent extends Component {
         if(!result){
             return null;
         }        
-        let nestedComponents = getNestedComponents(this.component.markup);
-        // Check if the component has nested components, make it available globally for preview.
-        if(nestedComponents.length>0){
-            // Save to window.
-            saveComponentsToWindow(nestedComponents);
-            // Reload.
-        }
-        let createdComponent = React.createElement(result);
 
         return (
             <div className={style.box}>
                 <h1>
-                    {createdComponent}
+                    {React.createElement(result)}
                 </h1>
             </div>
         );
