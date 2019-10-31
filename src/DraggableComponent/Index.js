80c8b6b9f4423ca6ff99ae81b7aa491a6bf29a5a
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-10-31 21:02:09 +0530

    Next export draggable Component from our tool

--- src/DraggableComponent/Index.js
+++ src/DraggableComponent/Index.js
@@ -7,13 +7,28 @@ import "./style.css";
 class DraggableComponent extends Component {
     constructor(props) {
         super(props);
-        this.component = this.props.component;
+        this.state = {
+            style: {
+                position: "fixed",
+                top: "400px",
+                left: "200px"
+            }
+        }
+    }
+
+    moveDiv(e){
+        let state = JSON.parse(JSON.stringify(this.state));
+        state.style.top = e.pageY +"px"
+        state.style.left = e.pageX +"px"
+        this.setState({
+            style : state.style
+        })
     }
 
     render() {
 
         return (
-            <div className="draggableComponentDaw">
+            <div draggable="true" id="move" onDragEnd={this.moveDiv.bind(this)} style={this.state.style} >
                 {this.props.children}
             </div>
         );
