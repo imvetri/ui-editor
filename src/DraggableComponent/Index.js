36e5e1a95a1724fad3e56c59bb6adcc4ecf2748e
imvetri <somethingelse@web.com>
2019-10-31 17:05:49 +0530

    tiny changes

---
+++ src/DraggableComponent/Index.js
@@ -0,0 +1,24 @@
+// Libraries.
+
+import React, { Component } from "react";
+
+import "./style.css";
+
+class DraggableComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.component = this.props.component;
+    }
+
+    render() {
+
+        return (
+            <div>
+                {props.children}
+            </div>
+        );
+    }
+
+}
+
+export default DraggableComponent;
