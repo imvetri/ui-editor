704f0dedaf77e4c6f73f84e9069cc6915bd466cd
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-11-02 13:49:24 +0530

    elements,preview,editor,settings are all draggable, next make it work. refactoring made it dancing, fix it

--- src/elements/Index.js
+++ src/elements/Index.js
@@ -6,13 +6,11 @@ import "./Style.css";
 
 // Components.
 
-import Editor from "../Editor";
-import Events from "../Events";
 import Element from "../Element";
 
 // Reducers.
 
-import {updateEvent, updateselectedIndex, saveElement,toggleEditor, setEditMode, onDelete, updateConfig} from "./Reducer"
+import {updateselectedIndex,toggleEditor, setEditMode, onDelete} from "./Reducer"
 
 // Events.
 
@@ -23,26 +21,14 @@ class Elements extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            element: {
-                name: "",
-                markup:"",
-                style: "",
-                state: "{ }",
-                events: []
-            },
-            elements: JSON.parse(localStorage.getItem("ui-editor")) || [],
-            selectedState: [],
-            editMode: false,
-            selectedIndex: -1
+            elements: this.props.elements,
+            editMode: false
         };
 
         this.onDelete = onDelete.bind(this);
-        this.updateEvent = updateEvent.bind(this);
         this.updateselectedIndex = updateselectedIndex.bind(this)
-        this.saveElement = saveElement.bind(this);
         this.toggleEditor = toggleEditor.bind(this);
         this.setEditMode = setEditMode.bind(this);
-        this.updateConfig = updateConfig.bind(this);
     }
 
 
@@ -61,9 +47,6 @@ class Elements extends Component {
                 onDelete = {this.onDelete.bind(this)}/>
         );
 
-    
-        const selectedElement = this.state.elements[this.state.selectedIndex] || this.state.element;
-
         
         return (
             <div className="elements">
@@ -75,21 +58,6 @@ class Elements extends Component {
                         {elementList}
                     </ul>
                 </div>
-                <Events 
-                    key={this.state.selectedIndex}
-                    element = {selectedElement}
-                    elements = {this.state.elements}
-                    onEventsUpdate ={this.updateEvent}
-                    onConfigUpdate={this.updateConfig}/>
-                    <Editor
-                        key = {Math.ceil(Math.random()*1000)}
-                        element = {selectedElement}
-                        name = {selectedElement.name}
-                        markup = {selectedElement.markup}
-                        style = {selectedElement.style}
-                        state = {selectedElement.state}
-                        onSave = {this.saveElement}
-                        />
             </div>
         );
     }
