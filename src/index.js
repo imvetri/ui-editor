b33fd6e5103e858f5008e4bd07998a7115fd34e0
imvetri <somethingelse@web.com>
2019-11-02 13:49:24 +0530

    elements,preview,editor,settings are all draggable, next make it work. refactoring made it dancing, fix it

--- src/index.js
+++ src/index.js
@@ -11,13 +11,27 @@ import "./Index/index.css";
 import Elements from "./Elements";
 import Preview from "./Preview";
 import DraggableComponent from "./DraggableComponent";
+import Editor from "./Editor";
+import Events from "./Events";
+
+// Reducers.
+import { updateEvent, updateConfig, saveElement } from "./elements/Reducer";
 
 
 class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            elements: JSON.parse(localStorage.getItem("ui-editor")) || [],
             components: [],
+            selectedIndex: -1,
+            element: {
+                name: "",
+                markup: "",
+                style: "",
+                state: "{ }",
+                events: []
+            },
             previewComponent: {
                 name: "",
                 markup: "",
@@ -30,6 +44,10 @@ class Index extends Component {
                 }]
             }
         }
+        this.updateConfig = updateConfig.bind(this);
+        this.updateEvent = updateEvent.bind(this);
+        this.saveElement = saveElement.bind(this);
+
     }
 
     updatePreview(element) {
@@ -39,12 +57,42 @@ class Index extends Component {
     }
 
     render() {
+
+        const selectedElement = this.state.elements[this.state.selectedIndex] || this.state.element;
+
         return (
             <div>
                 <div className="showBackground">
                     <DraggableComponent>
-                        <Elements createMode={false} onPreview={this.updatePreview.bind(this)} />
+                        <Elements
+                            elements={this.state.elements}
+                            createMode={false}
+                            onPreview={this.updatePreview.bind(this)}
+                        />
                     </DraggableComponent>
+                    <DraggableComponent>
+
+                        <Events
+                            key={this.state.selectedIndex}
+                            element={selectedElement}
+                            elements={this.state.elements}
+                            onEventsUpdate={this.updateEvent}
+                            onConfigUpdate={this.updateConfig}
+                        />
+
+                    </DraggableComponent>
+                    <DraggableComponent>
+                        <Editor
+                            key={Math.ceil(Math.random() * 1000)}
+                            element={selectedElement}
+                            name={selectedElement.name}
+                            markup={selectedElement.markup}
+                            style={selectedElement.style}
+                            state={selectedElement.state}
+                            onSave={this.saveElement}
+                        />
+                    </DraggableComponent>
+
                     <DraggableComponent>
                         <Preview component={this.state.previewComponent} />
                     </DraggableComponent>
