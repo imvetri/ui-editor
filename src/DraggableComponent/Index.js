47399f22f055ed1868eb6822521e62315c769cb9
imvetri <somethingelse@web.com>
2019-11-04 14:58:53 +0530

    Persists draggable component settings

--- src/DraggableComponent/Index.js
+++ src/DraggableComponent/Index.js
@@ -8,7 +8,7 @@ class DraggableComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            style: {
+            style: JSON.parse(localStorage.getItem(`ui-editor-settings-draggable-component-${this.props.children.type.name}`)) || {
                 position: "fixed",
                 top: "30px",
                 left: "200px"
@@ -17,16 +17,18 @@ class DraggableComponent extends Component {
     }
 
     moveDiv(e){
+
         let state = JSON.parse(JSON.stringify(this.state));
         state.style.top = e.pageY +"px"
         state.style.left = e.pageX +"px"
         this.setState({
             style : state.style
         })
+
+        localStorage.setItem(`ui-editor-settings-draggable-component-${this.props.children.type.name}`,JSON.stringify(state.style));
     }
 
     render() {
-
         return (
             <div draggable="true" id="move" onDragEnd={this.moveDiv.bind(this)} style={this.state.style} >
                 <span title="Move" className="move-handle">
