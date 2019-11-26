91ca17039b29aa977e738b5bbd612eba25fd82e1
imvetri <somethingelse@web.com>
2019-11-26 15:00:54 +0530

    Adds acceptanceCriteria. Next add prompts and fill up needed components in the UI-editor tab

---
+++ src/AcceptanceCriteria/Index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import  "./Style.css";
+
+class AcceptanceCriteria extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AcceptanceCriteria;
