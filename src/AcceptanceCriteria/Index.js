dc5a944c508f51f4cf61c2a07ee4e84229be7f3e
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-12-03 13:39:39 +0530

    adds slider

--- src/AcceptanceCriteria/Index.js
+++ src/AcceptanceCriteria/Index.js
@@ -5,13 +5,29 @@ import "./Style.css";
 class AcceptanceCriteria extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            open: true
+        };
+    }
+
+    openClose() {
+        this.setState({
+            open: !this.state.open
+        })
     }
 
     render() {
+
+        let slider = this.state.open ? "interact-horizontal first" : "slideIn";
+        let icon = this.state.open? "fa fa-arrow-left fa-xs" : "fa fa-arrow-right fa-xs";
+
         return (
             <div className="slider">
-                <div className="interact-horizontal first">
+                <div className={slider}>
+                </div>
+                <div className="snap">
+                    <input type="checkbox" checked={this.state.open} onChange={this.openClose.bind(this)}/>
+                    <span><i className={icon}></i></span>
                 </div>
             </div>
         );
