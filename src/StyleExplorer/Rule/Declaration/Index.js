b2567c2aee7ae85db9598cbfe0fc31a0b55d6b52
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-12-07 19:48:18 +0530

    style the style manager. Next add behaviour

--- src/StyleExplorer/Rule/Declaration/Index.js
+++ src/StyleExplorer/Rule/Declaration/Index.js
@@ -4,6 +4,11 @@ import React, { Component } from "react";
 
 import "./Style.css";
 
+// Components.
+
+import Property from "./Property";
+import Value from "./Value";
+
 class Declaration extends Component {
     constructor(props) {
         super(props);
@@ -11,8 +16,11 @@ class Declaration extends Component {
     }
 
     render() {
+
+        let declarations = Object.keys(this.props.declaration).map(key=> <span><span>{key}</span><span>{this.props.declaration[key]}</span></span>)
         return (
             <div className="container">
+                {declarations}
             </div>
         );
     }
