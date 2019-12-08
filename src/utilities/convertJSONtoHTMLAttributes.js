84c9ceb1fa3e378f1af641db53d47ab1dbd4c6c7
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-12-08 11:23:28 +0530

    changed

--- src/utilities/convertJSONtoHTMLAttributes.js
+++ src/utilities/convertJSONtoHTMLAttributes.js
@@ -1,10 +1,10 @@
 
 export function convertJSONtoHTMLAttributes(props) {
     let string = JSON.stringify(props);
-
+    Object.keys(props).forEach(prop=>props[prop]=`'${props[prop]}'`);
     // Dont consider the empty objects.
     if(string.length>2){
-        return JSON.stringify(props).split('"').map(item=> {
+        let list = JSON.stringify(props).split('"').map(item=> {
             if(item.includes("{") || item.includes("}")){
                 return undefined;
             }
@@ -15,7 +15,9 @@ export function convertJSONtoHTMLAttributes(props) {
                 return " "
             }
             return item;
-        }).filter(Boolean).join("")
+        }).filter(Boolean)
+
+        return list.join("");
     }
     return "";
 }
