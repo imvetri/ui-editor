49ec8ed740fd3254dc8eda6939940275e8486123
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-12-08 14:43:32 +0530

    onadd, add new rule

--- src/StyleExplorer/Rule/Declaration/Property/Index.js
+++ src/StyleExplorer/Rule/Declaration/Property/Index.js
@@ -10,10 +10,20 @@ class Property extends Component {
         this.state = Object.assign({}, this.props);
     }
 
+    updateSelector(e){
+        this.setState({
+            value: e.currentTarget.value
+        })
+    }
+
+    valueChanged(){
+        if(this.props.value!==this.state.value){
+            // then selector name changed
+        }
+    }
+
     render() {
-        return (
-            <span>{this.props.value}</span>
-        );
+        return  <input type="text" value={this.state.value} onBlur={this.valueChanged.bind(this)} onChange={this.updateSelector.bind(this)}/>
     }
 }
 
