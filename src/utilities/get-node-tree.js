ce6e796cc34dd4be66d49be9a92de6f7d37dad00
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-12-11 03:47:48 +0530

    writing is done

--- src/utilities/get-node-tree.js
+++ src/utilities/get-node-tree.js
@@ -3,6 +3,14 @@ import React from "react";
 // Why? Because importing React as variable at line#2 will be alterted by babel. Keep it as a property, babel will ignore it.
 window.React = React;
 window.Component = React.Component;
+window.saveVariants = function (componentFn, state) {
+
+    var components =JSON.parse(localStorage.getItem("ui-editor")) 
+    var component = components.find(component=>component.name === componentFn.name);
+    component.variants = component.variants || [];
+    component.variants.push(state);
+    localStorage.setItem("ui-editor", JSON.stringify(components));
+}
 
 export function getNodeTree(jsx, style, state, events) {
     let result, error;
