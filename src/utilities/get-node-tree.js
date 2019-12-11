58d71a60d9e4d46d539fa4ace4db8da93f6957ed
imvetri <somethingelse@web.com>
2019-12-11 15:36:57 +0530

    Bug fixed

--- src/utilities/get-node-tree.js
+++ src/utilities/get-node-tree.js
@@ -3,15 +3,36 @@ import React from "react";
 // Why? Because importing React as variable at line#2 will be alterted by babel. Keep it as a property, babel will ignore it.
 window.React = React;
 window.Component = React.Component;
-window.saveVariants = function (componentFn, state) {
+window.saveVariants = function (source, target, state) {
 
-    debugger;
     var components =JSON.parse(localStorage.getItem("ui-editor")) 
-    var component = components.find(component=>component.name === componentFn.name);
-    component.variants = component.variants || [];
-    component.variants.push(state);
-    component.variants = [...new Set(component.variants.map(JSON.stringify))].map(JSON.parse).filter(Boolean);
-    localStorage.setItem("ui-editor", JSON.stringify(components));
+
+    if(source===target){
+        var component = components.find(component=>component.name === source.name);
+        component.variants = component.variants || [];
+        component.variants.push(state);
+        component.variants = [...new Set(component.variants.map(JSON.stringify))].map(JSON.parse).filter(Boolean);
+        localStorage.setItem("ui-editor", JSON.stringify(components));
+    }
+    else{
+
+        // source is the parent and target is the child.
+        // Actual fix
+        // 1. find parent that has state[target] !== undefined
+
+        var sourceComponent = components.find(component=>component.name === source.name);
+        var targetComponent = components.find(component=>component.name === target.name);
+
+
+        // temporaryFix
+
+        sourceComponent.variants = sourceComponent.variants || [];
+        var obj = {};
+        obj[targetComponent.name] = state;
+        sourceComponent.variants.push(obj);
+        sourceComponent.variants = [...new Set(sourceComponent.variants.map(JSON.stringify))].map(JSON.parse).filter(Boolean);
+        localStorage.setItem("ui-editor", JSON.stringify(components));
+    }
 }
 
 export function getNodeTree(jsx, style, state, events) {
