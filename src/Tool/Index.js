df7a1425eab929f1cb66f54f214b99785a0d9ee2
imvetri <somethingelse@web.com>
2019-12-14 23:22:03 +0530

    cleanup

--- src/Tool/Index.js
+++ src/Tool/Index.js
@@ -71,7 +71,7 @@ class Tool extends Component {
     render() {
 
         const selectedElement = this.state.elements[this.state.selectedIndex] || this.state.element;
-        let nodeTree = getNodeTree(selectedElement.markup, selectedElement.style, JSON.parse(selectedElement.state), selectedElement.events);
+        let nodeTree = getNodeTree(selectedElement, selectedElement.markup, selectedElement.style, JSON.parse(selectedElement.state), selectedElement.events);
         
         return (
             <div>
