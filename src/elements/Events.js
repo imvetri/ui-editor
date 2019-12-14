62f897f684751b3665ed2acc3440c6eb69d5d65d
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-12-15 01:10:13 +0530

    preview removed

--- src/elements/Events.js
+++ src/elements/Events.js
@@ -1,17 +1,6 @@
-import { prepareElement} from "../utilities/codeGenerator/prepareElement";
 import { getNestedComponents } from "../utilities/nestedComponentSetup";
 import { getComponentString } from "../utilities/convert-to-react-component";
 
-
-
-export function publishDetails(componentName) {
-        
-    // Warning: Object.assign doesnt dupe the original object. It overrides only the values.
-    // May cause problem with reference types.
-    let element = JSON.parse(JSON.stringify(this.state.elements.find(component=>component.name===componentName)));
-
-    this.props.onPreview(prepareElement(element, element.name));
-}
 
 export function onExport() {
     let nestedComponents = getNestedComponents(this.state.elements[this.state.selectedIndex]);
