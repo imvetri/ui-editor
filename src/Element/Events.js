ecd2a4a9d67af603f4e995880305531962b708b9
imvetri <somethingelse@web.com>
2019-12-15 12:27:26 +0530

    delete element

--- src/Element/Events.js
+++ src/Element/Events.js
@@ -2,11 +2,6 @@ export function selectionChanged(e) {
     this.props.onSelectionChange(e);
 }
 
-export function deleteElement(e) {
-    let componentName = e.currentTarget.parentElement.parentElement.innerText.split("\n")[0];
-    this.props.onDelete(componentName);
-}
-
 export function handleDrag(e){
     e.dataTransfer.setData("component-name", event.target.getAttribute("id"));
 }
