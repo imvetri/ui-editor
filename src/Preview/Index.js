6557c4e34a1b8a842a22bab47d6bd8907d60c5a2
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-12-20 22:40:43 +0530

    preview has edit mode and interactive mode

--- src/Preview/Index.js
+++ src/Preview/Index.js
@@ -4,11 +4,45 @@ import React, { Component } from "react";
 
 import "./style.css";
 
+import { wrapTool } from "../utilities/wrapTool";
+
 import DynamicComponent from "../DynamicComponent";
 
 class Preview extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            events: {}
+        }
+    }
+
+    setToEditMode() {
+        // Set overlay.
+        this.setState({
+            events: {
+                onMouseOver: this.showHover.bind(this),
+                onClick: this.showTools.bind(this)
+            }
+        })
+    }
+
+    setToInteractMode() {
+        // Remove overlay.
+        this.setState({
+            events: {}
+        })
+    }
+
+    showHover(e){
+        wrapTool(e)
+        e.stopPropagation();
+        console.log(e)
+    }
+
+    showTools(e){
+        // TODO : Change the markup to show the element.
+        e.target.outerHTML = `<span><i className="fas fa-save"></i>${e.target.outerHTML}</span>`
+        debugger;
     }
 
     render() {
@@ -20,7 +54,11 @@ class Preview extends Component {
                 <div className="title">
                     Preview
                 </div>
-                <DynamicComponent key={randomKey} component={this.props.component}/>
+                <div>
+                    <button onClick={this.setToEditMode.bind(this)}><i className="fas fa-file-export"></i>Edit</button>
+                    <button onClick={this.setToInteractMode.bind(this)}><i className="fas fa-file-export"></i>Interact</button>
+                </div>
+                <DynamicComponent key={randomKey} component={this.props.component} events={this.state.events}/>
             </div>
         );
     }
