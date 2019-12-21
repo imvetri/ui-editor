1c20706fb0c961652fc921abb87da79d4286909d
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-12-21 19:58:26 +0530

    cleanup waste code

--- src/DynamicComponent/Index.js
+++ src/DynamicComponent/Index.js
@@ -6,7 +6,6 @@ import {createStylesheet} from "../utilities/jsxTranspiler/create-stylesheet";
 
 import { getNestedComponents, saveComponentsToWindow } from "../utilities/nestedComponentSetup";
 import { writeComponent } from "../utilities/localStorage";
-import { wrapTool } from "../utilities/wrapTool";
 
 
 import "./style.css";
@@ -20,33 +19,6 @@ class DynamicComponent extends Component {
         
         createStylesheet(this.state.component.style);
     }
-
-    preventDefault(e){
-        wrapTool(e)
-        e.preventDefault();
-    }
-
-    onDrop(e){
-        e.preventDefault();
-        var child = e.dataTransfer.getData("component-name");
-        /**
-         * 1. Fetch the uuid of the drop target.
-         * 2. Replace the child component name next to the uuid tag of the parent.idMarkup.
-         */
-        var uuid = e.target.getAttribute("data-uuid");
-        var parent = this.state.component;
-        parent.idMarkup = parent.idMarkup.replace(`"${uuid}">`,`"${uuid}"><${child}/>`)
-
-        /**
-         * before write, fetch idMakrup with child components into component.markujp.
-         */
-        writeComponent(parent, true);
-        // 5. Trigger reload.
-        this.setState({
-            component: parent
-        })
-    }
-
 
     render() {
 
@@ -63,7 +35,7 @@ class DynamicComponent extends Component {
         }
 
         return (
-            <div onDrop={this.onDrop.bind(this)} onDragOver={this.preventDefault.bind(this)} {...this.props.events}>
+            <div {...this.props.events}>
                 {React.createElement(window[this.state.component.name])}
             </div>
         );
