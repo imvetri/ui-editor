6325cbdbb252cd31614edf1713c48f378a847f97
Shanmugam, Vetrivel <VShanmugam@mast.com>
2019-12-22 17:54:10 +0530

    sdfomg

--- src/utilities/localStorage/index.js
+++ src/utilities/localStorage/index.js
@@ -107,7 +107,6 @@ export function writeComponent(parent, idMarkupModified) {
         let components = readData( "ui-editor");
         let index = components.findIndex(comp=>comp.name === parent.name);
         if(idMarkupModified){
-            debugger;
             parent.markup = noIdMarkup(parent.idMarkup);
         }
         components[index] = parent;
