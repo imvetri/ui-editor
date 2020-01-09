51fcf1e1bc8d1ef72390ba47377f6efd34d22a47
imvetri <svetrivel.91@gmail.com>
2020-01-09 13:52:15 +0530

    sets db

--- src/utilities/indexedDB/indexeDB.js
+++ src/utilities/indexedDB/indexeDB.js
@@ -2,14 +2,8 @@ window.iDB = "";
 import { MindexedDB } from "./MindexedDB";
 
 window.onload = function (){
-    debugger;
-    window.iDB = new MindexedDB("testDB", {testStore: "name"});
-    window.iDB.connect().then(function() {
-        iDB.put("testStore", {name: "Ralf", species: "Dog"});
-        iDB.put("testStore", {name: "Fred", species: "Cat", numbers: [1,2,3]});
-        iDB.get("testStore", "Fred").then(function(animal){console.log(animal)});
-       // iDB.close();
-    })
+    window.iDB = new MindexedDB("uiEditor", {uiEditor: "name"});
+    window.iDB.connect();
 }
 
 
