bbee4ca85afadad88c8f443f9ef7113e3d74dc2d
imvetri <svetrivel.91@gmail.com>
2020-02-09 18:48:58 +0530

    adds animation

--- src/Folder/Index.js
+++ src/Folder/Index.js
@@ -10,16 +10,56 @@ class Folder extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            status: "fa fa-folder",
+            newFolderClass: "newFolder"
         };
+    }
+
+    openFolder(){
+        this.setState({
+            status: "fa fa-folder-open"
+        })
+    }
+
+    closeFolder(){
+        this.setState({
+            status: "fa fa-folder"
+        })
+    }
+
+    toggleFolder(){
+        this.state.status === "fa fa-folder"?  this.openFolder() : this.closeFolder();
+    }
+
+    dropHandler(ev) {
+        ev.preventDefault();
+
+        this.setState({
+            newFolderClass: "newFolder"
+        })
+        console.log("Drop");
+    }
+
+    dragOverHandler(ev) {
+        ev.preventDefault();
+        this.setState({
+            newFolderClass: "newFolder dragOver"
+        })
+        console.log("Drag");
+    }
+
+    dragLeaveHandler(e) {
+        console.log("drag");
+        this.setState({
+            newFolderClass: "newFolder"
+        })
     }
 
     render() {
 
         return (
-        <div className="newFolder">
-            <i class="fa fa-folder" aria-hidden="true"></i>
-            <i class="fa fa-folder-open" aria-hidden="true"></i>
-
+        <div className={this.state.newFolderClass} onDrop={this.dropHandler.bind(this)} onDragOver={this.dragOverHandler.bind(this)} onDragLeave={this.dragLeaveHandler.bind(this)} >
+            <i class={this.state.status} onClick={this.toggleFolder.bind(this)} aria-hidden="true"></i>
             <input type="text" className="folder" autoFocus={true} placeholder="Enter folder name"/>
         </div>
         );
