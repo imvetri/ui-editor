c71f7c8fd7226d37f7e141ae8f37409596038da6
imvetri <svetrivel.91@gmail.com>
2020-02-09 15:59:16 +0530

    fix commit

--- src/utilities/jsxTranspiler/create-stylesheet.js
+++ src/utilities/jsxTranspiler/create-stylesheet.js
@@ -1,15 +1,19 @@
 // Implement stylesheet cleanup
 
+/**
+ * 
+ * Asset in style sheet syntax - $asset
+ */
+
 export function createStylesheet(style, name) {
 
     // Check if style has $assets
-    if(style.includes("assets")){
+    while(style.includes("$assets")){
         // Replace it with asset blob url
-        debugger;
-        let asset = style.split('["')[1].split(`]`)[0].split("");
+        let asset = style.split("['")[1].split(`]`)[0].split("");
         asset.pop();
-       asset =  asset.join("");
-        style = style.replace(`assets["${asset}"]`, `url(${window.assets[asset]})`)
+        asset =  asset.join("");
+        style = style.replace(`$assets['${asset}']`, `url(${window.assets[asset]})`)
     }
     let toDelete = [...document.querySelectorAll("[data-component-name='ParentComponent']")];
     toDelete.forEach(item=>{
