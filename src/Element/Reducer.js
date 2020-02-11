73f45e59777a658a64e13d8366c993e05171067f
imvetri <svetrivel.91@gmail.com>
2020-02-11 22:27:27 +0530

    removes selectedIndex. Show components inside folders

--- src/Element/Reducer.js
+++ src/Element/Reducer.js
@@ -1,14 +1,14 @@
 // Dependencies.
 
-export function updateselectedIndex (e) {
+export function updateSelectedComponent (e) {
     // Find the element from state that matches the currently selected element.
-    let selectedIndex = Number(e.target.getAttribute("index"));
+    let componentName = e.currentTarget.innerText.split("\n")[0];
+    let selectedComponent = this.state.elements.find(component=>component.name===componentName);
 
+    window.selectedcomponentname = selectedComponent.name;
     // Update the state with selectedElement.
     this.setState({
-        selectedIndex,
-        name: this.state.elements[selectedIndex].name,
-        markup: this.state.elements[selectedIndex].markup
+        selectedComponent: selectedComponent
     })
 
 }
