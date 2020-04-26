b6e5f13a0197cc49a517f0b0ec3ea9c8fd43374d
imvetri <svetrivel.91@gmail.com>
2020-04-26 14:33:35 +0530

    Next add height and width for different displays

--- src/Preview/Index.js
+++ src/Preview/Index.js
@@ -14,7 +14,9 @@ class Preview extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            component: this.props.component
+            component: this.props.component,
+            showControls: true,
+            display: "mobile"
         }
     }
 
@@ -24,14 +26,52 @@ class Preview extends Component {
         })
     }
 
+    switchTablet() {
+        this.setState({
+            display: "tablet"
+        })
+    }
+
+    switchMobile() {
+        this.setState({
+            display: "mobile"
+        })
+    }
+
+    switchDesktop(){
+        this.setState({
+            display: "desktop"
+        })
+    }
+
+    showControls() {
+        this.setState({
+            showControls: true
+        })
+    }
+
+    hideControls() {
+        this.setState({
+            showControls: false
+        })
+    }
+
     render() {
 
         // Helps to rerender when changes to markup/events are made to the component and preview them.
         let randomKey = this.props.component.id*(~~(Math.random()*10));
+        let classes = this.state.showControls ? 'Controls' : 'Controls hideControls'
+        classes = classes + " " + this.state.display
+
         return (
-            <div className="container preview">
+            <div className="container preview" onMouseEnter={this.showControls.bind(this)} onMouseLeave={this.hideControls.bind(this)}>
                 <div className="title">
                     Preview
+                    <div className={classes}>
+                        <button onClick={this.switchTablet.bind(this)}><i className="fa fa-tablet-alt"></i></button>
+                        <button onClick={this.switchMobile.bind(this)}><i className="fa fa-mobile-alt"></i></button>
+                        <button onClick={this.switchDesktop.bind(this)}><i className="fa fa-desktop"></i></button>
+                    </div>
                 </div>
                 <div>
                     <div className="dynamicComponent">
