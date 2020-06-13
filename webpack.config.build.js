6830994d7fc181c6c21e8ea6a8f5bbb4ee5d4278
imvetri <svetrivel.91@gmail.com>
2020-06-13 10:05:19 +0530

    slick build config

---
+++ webpack.config.build.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+    template: "./src/Index/index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    output: {
+        path: __dirname + '/docs'
+    },
+    stats: 'errors-only',
+    devServer: {
+        clientLogLevel: 'silent'
+      },
+    optimization: {
+        splitChunks:{
+            cacheGroups: {
+                modules: {
+                    name: 'modules',
+                    chunks: 'all',
+                    test: /node_modules/
+                },
+                common: {
+					test: /src/,
+					name: `src`,
+					chunks: "all"
+                }
+            }
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    
+                    {
+                        "loader": "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [htmlWebpackPlugin]
+};
