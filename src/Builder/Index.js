446b20d834e6461e950b5e4c85126b06d83ff725
imvetri <vetrivel.shanmugam@sap.com>
2021-05-22 10:52:45 +0530

    somehting

--- src/Builder/Index.js
+++ src/Builder/Index.js
@@ -27,8 +27,16 @@ class Builder extends Component {
 
     render() {
         /**
-         * Draw is on by default.
-         * On click on select. On select, enable Move, Resize, Delete, copy, Save, Edit
+         * when Draw is on - Disable  - Move, Resize, Delete, copy, Save, Edit
+         * when Select is on - Enable - Move, Resize, Delete, copy, Save, Edit
+         * when Interact is on - 
+         * 
+         * There are three modes, Draw, select and interact. 
+         * 
+         * 1. Draw - When in draw mode, rectangles can be created
+         * 2. Select - When in select mode, Rectangle can be selected. After selection it can be moved, resized, Deleted, copied, saved
+         * 3. Event - Helps to add event interaction
+         * 4. Interact - Helps to preview the changes.
          */
         return (
         <div className="builder">
@@ -42,6 +50,7 @@ class Builder extends Component {
                 <button className={this.state.mode==="Save"?"mode":""} onClick={this.changeMode.bind(this)}><i class="fas fa-save"></i>Save</button>
                 <button className={this.state.mode==="Edit"?"mode":""} onClick={this.changeMode.bind(this)}><i class="fas fa-edit"></i>Edit</button>
                 <button className={this.state.mode==="Events"?"mode":""} onClick={this.changeMode.bind(this)}><i class="fas fa-bolt"></i>Events</button>
+                <button className={this.state.mode==="Interact"?"mode":""} onClick={this.changeMode.bind(this)}><i class="fas fa-bolt"></i>Interact</button>
             </div>
             <Div mode={this.state.mode}></Div>
         </div>
