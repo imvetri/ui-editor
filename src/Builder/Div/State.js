d6d5e7bbc8f0cf4c4eab5317d224960f29bb6d61
Shanmugam <vetrivel.shanmugam@sap.com>
2021-10-08 17:42:24 +0530

    Try selection mode

--- src/Builder/Div/State.js
+++ src/Builder/Div/State.js
@@ -13,63 +13,26 @@ module.exports = {
     },
     "type": "p",
     "children": [
-        {
-            "style": {
-                "position": "absolute",
-                "top": 166,
-                "left": 1648,
-                "height": "262px",
-                "width": "430px",
-                "borderWidth": "1px",
-                "borderStyle": "solid",
-                "borderColor": "green",
-                "resize": "",
-                "overflow": ""
-            },
-            "type": "Img",
-            "source": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-            "children": [],
-            "id": "div66742",
-            "mode": "Draw"
-        },
-        {
-            "style": {
-                "position": "absolute",
-                "top": 37,
-                "left": 644,
-                "height": "229px",
-                "width": "818px",
-                "borderWidth": "1px",
-                "borderStyle": "solid",
-                "borderColor": "green",
-                "resize": "",
-                "overflow": ""
-            },
-            "type": "Div",
-            "children": [],
-            "id": "div38705",
-            "mode": "Draw"
-        },
-        {
-            "style": {
-                "position": "absolute",
-                "top": 437,
-                "left": 644,
-                "height": "229px",
-                "width": "818px",
-                "borderWidth": "1px",
-                "borderStyle": "solid",
-                "borderColor": "green",
-                "resize": "",
-                "overflow": ""
-            },
-            "type": "img",
-            "source":"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-            "children": [],
-            "id": "div38705",
-            "mode": "Draw"
-        }
-    ],
+      {
+          "style": {
+              "position": "absolute",
+              "top": 166,
+              "left": 1648,
+              "height": "262px",
+              "width": "430px",
+              "borderWidth": "1px",
+              "borderStyle": "solid",
+              "borderColor": "green",
+              "resize": "",
+              "overflow": ""
+          },
+          "type": "Img",  
+          "source": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+          "children": [],
+          "id": "div66742",
+          "mode": "Draw"
+      }
+  ],
     "mode": "Draw",
     "grabbing": false,
     "origin": false,
