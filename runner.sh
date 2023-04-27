3e1db5fab41a1fb1920f887e7413553820603f01
ivetrivel <vetrivel_shanmugam@discovery.com>
2023-04-27 17:27:53 +0530

    editable label for new custom nodes works smooth

---
+++ runner.sh
@@ -0,0 +1,15 @@
+git status
+npm run build
+git status
+git add .
+git commit -m "editable label for new custom nodes works smooth"
+git branch
+git push
+cd offgrid
+ls
+cd ui-editor
+nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"\n[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm
+nvm use 14
+npm start
