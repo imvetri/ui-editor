678dcbf789ac19d82996c8288f7eeadd6d7d7218
Vetrivel Shanmugam <svetrivel.91@gmail.com>
2023-12-07 11:33:21 +0000

    draw to code

--- src/index.js
+++ src/index.js
@@ -30,24 +30,56 @@ class Index extends Component {
                 style: "",
                 state: "{ }",
                 events: []
-            }
+            },
+            drawelements: []
         }
-
+        window.count=0;
     }
 
     onExcalidrawChange (elements, appState, files) {
-        console.log(elements, appState, files)
+        count++;
+        console.log(count)
+        if(count>500){
+            console.log(elements, appState, files)
+
+            this.setState({drawelements: elements})
+            count=0;
+        }
     }
 
+    onExcalidrawFinish() {
+        console.log("FINISH")
+    }
+
+    onExcalidrawStart(){
+        console.log("START")
+    }
     render() {
         const randomKey = Math.ceil(Math.random() * 1000);
         return (
             <div>
+                {this.state.drawelements.map(element=>{
+                    return <div className="centerItem">{React.createElement("div",{
+                        className: "lalalala",
+                        style:{
+                            zIndex: 3,
+                            width:element.width,
+                            height:element.height,
+                            position:"fixed",
+                            top: element.x,
+                            left:element.y,
+                            backgroundColor: "red"
+                        }
+
+                    } )}</div>
+                })}
                 <Markup markup={this.state.component.markup} key={randomKey}></Markup>
                 <Style style={this.state.component.style} key={randomKey}></Style>
                 <State state={this.state.component.state} key={randomKey}></State>
-                <div style={{ height: "100vh", width: "50vw" }}>
-                    <Excalidraw onChange={this.onExcalidrawChange}/>
+                <div style={{ height: "90vh", width: "50vw" }}>
+                    <Excalidraw onChange={this.onExcalidrawChange.bind(this)}
+                                onPointerUp={this.onExcalidrawFinish.bind(this)}
+                                onPointerDown={this.onExcalidrawStart.bind(this)}/>
                 </div>
             </div>
         );
