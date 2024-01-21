d2b996be786848f5db27547a6778e1af6bb65d45
Vetrivel Shanmugam <svetrivel.91@gmail.com>
2024-01-21 22:05:48 +0530

    from scratch

--- src/Window/config.js
+++
@@ -1,9 +0,0 @@
-
-var config = {
-    Events: {"collapsed":false,"top":400,"left":941},
-    State: {"collapsed":false,"top":599,"left":892},
-    Markup:{"collapsed":true,"top":175,"left":800},
-    Style:{"collapsed":true,"top":406,"left":877}
-}
-
-export default config;
