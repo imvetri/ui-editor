1401e4ff8251ee5aa1797d1c1ef8a897d580451f
Vetrivel Shanmugam <svetrivel.91@gmail.com>
2025-05-03 02:23:00 +0530

    Add files via upload

---
+++ boxtree.js
@@ -0,0 +1,150 @@
+var dChildren = boxes || [];
+var shows = boxes.filter(box=>box?.show)?.map(s=>s.show) || []
+var types = boxes[0].types || []
+var flows = boxes.filter(box=>box.flows).map(d=>d.flows).flat()
+function boxtree(){
+	function findChildren(child){
+		return dChildren.filter(c=>c!==child&&c.left>=child.left && c.top>=child.top&&c.height+c.top<=child.height+child.top&& c.left+c.width<=child.left+child.width )
+	}
+	dChildren.forEach((child,i)=>{
+		child.index = i ;
+		child.boxChildren = findChildren(child).map(e=>dChildren.findIndex(child=>child===e))
+	})
+	var leaves = dChildren.filter(child=>child.boxChildren.length===0).map(e=>dChildren.findIndex(c=>c===e))
+	leaves.forEach(leaf=>{
+		dChildren[leaf].parents = dChildren.filter(parent=>{
+			return parent.boxChildren.find(boxChild=>boxChild===leaf)
+		}).map(e=>dChildren.findIndex(c=>c===e))
+	})
+	// for each leaf, find hierarchy of parents
+	leaves.forEach(leaf=>{
+		dChildren[leaf].parents = dChildren[leaf].parents.map(e=>dChildren[e]).sort((a,b)=>a.volume-b.volume).map(e=>dChildren.findIndex(c=>c===e))
+	})
+	// for each leaf, and for each parent, update immediate parent
+	leaves.forEach(leaf=>{
+		dChildren[leaf].immediateParent=dChildren[leaf].parents[0];
+		// update immediateParent for other parents
+		for(var i=0;i<dChildren[leaf].parents.length-1;i++){
+			dChildren[dChildren[leaf].parents[i]].immediateParent = dChildren[leaf].parents[i+1];
+		}
+		// update immediateChildren for parents
+		for(var i=dChildren[leaf].parents.length-1;i>0;i--){
+			dChildren[dChildren[leaf].parents[i]].immediateChildren = dChildren[dChildren[leaf].parents[i]].immediateChildren|| []
+			dChildren[dChildren[leaf].parents[i]].immediateChildren.push(dChildren[leaf].parents[i-1])
+		}
+		if(dChildren[dChildren[leaf].parents[0]]){
+			dChildren[dChildren[leaf].parents[0]].immediateChildren = dChildren[dChildren[leaf].parents[0]].immediateChildren || [];
+			dChildren[dChildren[leaf].parents[0]].immediateChildren.push(leaf)
+		}
+	})
+	// dedupe immediatechildren
+	dChildren.forEach(d=>{
+		if(d.immediateChildren){
+			d.immediateChildren = [... new Set(d.immediateChildren)]
+		}
+	})
+}
+boxtree()
+var f;
+function addFocus(){
+	f&&f.remove();
+	f=document.createElement("div")
+	f.id="focus"
+	var eleHeight = Number(dChildren[currentParent].element.style.height.split("px")[0])
+	var eleWidth = Number(dChildren[currentParent].element.style.width.split("px")[0])
+	var firstpointx, firstpointy;
+	firstpointx = document.body.scrollLeft
+	firstpointy = document.body.scrollTop
+	var secondpointx, secondpointy
+	secondpointy = ~~(zoom*Number(dChildren[currentParent].element.style.top.split("px")[0])-window.innerHeight/2+eleHeight/2);
+	secondpointx = ~~(zoom*Number(dChildren[currentParent].element.style.left.split("px")[0])-window.innerWidth/2+eleWidth/2)
+	firstpointx, firstpointy, secondpointx, secondpointy;
+	// find the direction - above, below, right, left.
+	function findDirection(){
+		// once the values are same, dont touch it
+		if(secondpointx!==firstpointx){
+			if(secondpointx>firstpointx) firstpointx=~~(firstpointx+1);
+			else firstpointx=~~(firstpointx-1)
+		}
+		if(secondpointy!==firstpointy){
+			if(secondpointy>firstpointy) firstpointy= ~~(firstpointy+1)
+			else firstpointy=~~(firstpointy-1)
+		}
+	}
+	findDirection()
+	while(!(firstpointx==secondpointx && firstpointy==secondpointy)){
+		// push it to queue
+		findDirection()
+	}
+	document.body.scrollTo({
+   		top:firstpointy,   		
+		left:firstpointx,
+		behavior: "smooth"
+	})
+	var rect = dChildren[currentParent].element.getBoundingClientRect()
+	f.style.position="absolute"
+	f.style.left=dChildren[currentParent].element.offsetLeft;
+	f.style.top=dChildren[currentParent].element.offsetTop;
+	f.style.width=eleWidth
+	f.style.height=eleHeight 
+	f.style.boxSizing="border-box"
+	f.style.border="3px dashed black"
+	f.style.zIndex=1;
+	document.body.appendChild(f)
+	f.element=f
+}
+function getChild(div){
+	return div.immediateChildren[0];
+}
+var currentParent=0;
+var currentChild=0;
+function nextChild(){
+	currentChild = currentChild+1;
+	if(currentChild< dChildren[dChildren[currentParent].immediateParent].immediateChildren.length)
+		currentParent = dChildren[dChildren[currentParent].immediateParent].immediateChildren[currentChild]
+	else
+		currentChild = currentChild-1
+}
+function prevChild(){
+	currentChild = currentChild-1
+	if(currentChild>=0){}
+	else{
+		currentChild = 0
+	}
+	currentParent = dChildren[dChildren[currentParent].immediateParent].immediateChildren[currentChild]
+}
+function aboveParent(){
+	currentChild=0;
+	if(dChildren[currentParent].immediateParent>=0)
+		currentParent=dChildren[currentParent].immediateParent;
+}
+function belowParent(){
+	currentChild=0;
+	if(dChildren[currentParent].immediateChildren && dChildren[currentParent].immediateChildren.length!=0)
+		currentParent=dChildren[currentParent].immediateChildren[0]
+}
+function move(direction, target){
+	if(direction==="right"){
+		target.element.style.left = Number(target.element.style.left.split("px")[0])+speed;
+	}
+	if(direction==="left"){	
+		target.element.style.left = Number(target.element.style.left.split("px")[0])-speed;
+	}
+	if(direction==="up"){
+		target.element.style.top = Number(target.element.style.top.split("px")[0])-speed;
+	}
+	if(direction==="down"){
+		target.element.style.top = Number(target.element.style.top.split("px")[0])+speed;
+	}
+	target.immediateChildren && target.immediateChildren.forEach((c)=>{
+		move(direction,dChildren[c])
+	})
+}
+function size(command, element){
+	if(command==="increasewidth"){element.style.width=Number(getComputedStyle(element).width.split("px")[0])+speed}
+	if(command==="increaseheight"){element.style.height=Number(getComputedStyle(element).height.split("px")[0])+speed}
+	if(command==="decreasewidth"){element.style.width=Number(getComputedStyle(element).width.split("px")[0])-speed}
+	if(command==="decreaseheight"){element.style.height=Number(getComputedStyle(element).height.split("px")[0])-speed}
+}
+var l = dChildren.filter(child=>child.boxChildren.length===0).map(e=>dChildren.findIndex(c=>c===e))
+l = l.map(i=>dChildren[i])
---
+++ render.js
@@ -0,0 +1,238 @@
+var changed;
+let tableindexchange;
+function render(box) {
+    var d = document.createElement(box.type)
+    d.style.top = box.top
+    d.style.left = box.left
+    d.id = box.id
+    d.style.height = box.height
+    d.style.width = box.width
+    d.src = box.src
+    d.style.position = "absolute"
+    d.style.border = "1px solid black"
+    if (Object.keys(box?.decorate || {}).length > 0) {
+        Object.keys(box?.decorate || {}).forEach(de => {
+            d.style[de] = box.decorate[de]
+        }
+        )
+    }
+    if (box.storage) {
+        d.style.height = "fit-content"
+        d.style.minHeight = "30px"
+
+    }
+    box.storage && box.storage.forEach( (t, id) => {
+        var i = document.createElement("span")
+        if (typeof t === "object") {
+            i.innerHTML = `${id} <br/>` + Object.keys(t).map( (e) => {
+                return `<span>${e} : </span><span>${t[e]}</span>`
+            }
+            ).join("<br/>")
+        } else {
+            i.innerText = t;
+        }
+        i.style = "border:1px solid black; padding:3px;display:block;"
+        d.appendChild(i)
+    }
+    )
+    if (mode == "annotate" || mode === "wireframe") {
+        box.rcvalues && box.rcvalues.forEach(t => {
+            var i = document.createElement("span")
+            i.innerText = t;
+            i.style = "border:1px solid black; padding:3px;display:block;"
+            d.appendChild(i)
+        }
+        )
+    }
+    if (d.src && mode !== "result") {
+        var i = document.createElement("img")
+        i.src = d.src
+        d.appendChild(i)
+    }
+    if (mode === "result") {
+        if (box.rtype === "content") {
+            if (box.rctype === "Text") {
+                d.innerText = box.rcvalue
+            }
+            if (box.rctype === "Image") {
+                var i = document.createElement("img")
+                i.src = box.rcvalue
+                d.appendChild(i)
+            }
+            if (box.rctype === "Data source" && !box.storage) {
+                d.innerText = dChildren[box.rcvalue].storage[params.storageindex || 0][box.rcprop]
+            }
+            if (box.rctype === "Input" && box.boxChildren.length == 0) {
+                if (box.flow) {
+                    storage(box)
+                    // if first render
+                    // if rerender source is table index change 
+                    if(boxrendercount>0&&tableindexchange){
+                        box.rcvalue = boxes[box.flow.to].storage[params.storageindex][box.rcname || box.rctypename]
+                        tableindexchange=false;
+                    }
+                    if (boxrendercount === 1) {
+                        box.rcvalue = boxes[box.flow.to].storage[params.storageindex][box.rcname || box.rctypename]
+                    } else {
+                        box.rcvalue = box.rcvalue
+                    }
+                    if (box.rcvalue === "" && boxrendercount === 1) {
+                        box.rcvalue = boxes[box.flow.to].storage[params.storageindex][box.rcname || box.rctypename]
+                    }
+                    else {
+                        box.rcvalue = box.rcvalue
+                    }
+                    // if rerender
+                }
+                var i = document.createElement('input')
+                i.value = box.rcvalue;
+                i.style.width = d.style.width;
+                i.style.height = d.style.height;
+                i.style.border = d.style.border;
+                i.addEventListener("input", (e) => {
+                    box.rcvalue = e.target.value;
+                    // save value to storage only if valid validator
+                    // else save to rcvalue
+                    var valid = validator(box);
+                    changed = box;
+                    rresult()
+                    changed?.element?.children[0]?.focus()
+                }
+                )
+                if (box.flow) {
+                    let b= boxes[box.flow.to];
+                    if (b.storagemovenext && b.storagemoveprev) {
+                        setTimeout( () => {
+                            boxes[b.storagemovenext].element.addEventListener("click", () => {
+                                params.storageindex = params.storageindex == b.storage.length - 1 ? 0 : params.storageindex + 1;
+                                tableindexchange=true;
+                                rresult();
+                            }
+                            )
+                            boxes[b.storagemoveprev].element.addEventListener("click", () => {
+                                params.storageindex = params.storageindex == 0 ? b.storage.length - 1 : params.storageindex - 1
+                                tableindexchange=true;
+                                rresult();
+                            }
+                            )
+                        }
+                        , 0)
+                    }
+                    if (box.flow.on === "ENTER") {
+                        i.addEventListener("keypress", e => {
+                            if (e.key == "Enter") {
+                                var valid = validator(box);
+                                if (valid) {
+                                    savestorage(box);
+                                }
+                                showbox(box);
+
+                            }
+                        }
+                        )
+                    }
+                    if (box.flow.on === "CLICK") {
+                        setTimeout( () => {
+                            boxes[box.flow.click].element.addEventListener("click", () => {
+                                var valid = validator(box);
+                                if (valid) {
+                                    savestorage(box);
+                                }
+                                showbox(box);
+                            }
+                            )
+                        }
+                        , 0)
+                    }
+                }
+                d.appendChild(i)
+            }
+        }
+    }
+    d.json = box
+    document.body.appendChild(d)
+    box.element = d;
+}
+function clean() {
+    dChildren.forEach(d => {
+        delete d.boxChildren
+        delete d.immediateChildren
+        delete d.immediateParent
+        delete d.parents
+        delete d.element
+    }
+    )
+    var cleanbox = JSON.parse(localStorage.boxes)
+    cleanbox.forEach( (e, i) => {
+        boxes[i].top = e.top;
+        boxes[i].left = e.left;
+    }
+    )
+}
+function storage(box) {
+    boxes[box.flow.to].storage = boxes[box.flow.to].storage || []
+    boxes[box.flow.to].storage[params.storageindex] = boxes[box.flow.to].storage[params.storageindex] || {};
+}
+function savestorage(box) {
+    boxes[box.flow.to].storage[params.storageindex][box.rcname || box.rctypename] = box.rcvalue
+    clean()
+    box.rcvalue = ""
+    localStorage.boxes = JSON.stringify(boxes)
+}
+
+function showbox(box) {
+    clean()
+
+    boxtree()
+    var valid = validator(box);
+    var movement = box.shows?.filter(s => s.on === (valid ? "VALID" : "INVALID")) || []
+    movement.forEach(movement => {
+        movefromto(boxes[movement.this], boxes[movement.here])
+    }
+    )
+    boxtree()
+    rresult()
+
+}
+
+function resetBoxtree() {
+    dChildren.forEach(d => {
+        delete d.boxChildren
+        delete d.immediateChildren
+        delete d.immediateParent
+        delete d.parents
+    }
+    )
+    boxtree()
+}
+
+function validator(box) {
+    // type of type type
+    if (box.rctypename.includes(".")) {
+        var type = types.find(t => t.name == box.rctypename.split(".")[0])
+        var inputs = boxes.filter(b => b?.flow?.to === box?.flow?.to)
+        var result = boxes[type.source].storage.every(value => {
+            // no storage should have save value as in inputs
+            return inputs.every(inp => {
+                return value[inp.rctypename.split(".")[1]] !== inp.rcvalue
+            }
+            )
+        }
+        )
+        return result
+    }
+    // type of type value
+    var type = types.find(t => t.name == box.rctypename)
+    if (!type?.rule)
+        return true;
+
+    var values = boxes[type.source].storage.map(s => s[type.name]);
+    var value = box.rcvalue;
+    if (type.rule === "IN") {
+        return values.includes(value)
+    }
+    if (type.rule === "NOT IN") {
+        return !values.includes(value)
+    }
+    return true
+}
